4f1fc0cad83a83e3cbd30b7f93bc37a3305eab22
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-09-20 14:44:23 -0400

    Aaaaaah

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
   var month = time.getMonth();
 
 if (day ==  21 && month == 8) {
-    document.getElementById("H").innerHTML = "<h1>Happy Birthday!</h1>"
+    document.getElementById("H").innerHTML = "<h1>Happy Birthday αqualine!</h1>"
   } else
   {
-    document.getElementById("H").innerHTML = "<h1>It's not your birthday</h1>"
+    document.getElementById("H").innerHTML = "<h1>It's not αqualine's birthday</h1>"
   }
