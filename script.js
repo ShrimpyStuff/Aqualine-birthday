281fbafb3b629c258fc0512204805582f6e7164d
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-09-21 10:17:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
   var date = new Date();
   var time = date.toLocaleString("en-CA");
   var day = date.getDate().toLocaleString("en-CA");
-  var month = date.getMonth();
-console.log(time);
+  var month = date.getMonth().toLocaleString("en-CA");
 if (day ==  21 && month == 8) {
     document.getElementById("H").innerHTML = "<h1>Happy birthday αqualine!</h1>"
     document.title = "Happy birthday!";
