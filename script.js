62a7e1b520d32888e713076fb12bce80fa7c07e1
ShrimpyStuff <52041627+ShrimpyStuff@users.noreply.github.com>
2020-10-29 19:43:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,11 @@
   var day = date.getDate().toLocaleString("en-CA", {timeZone: "America/Toronto"});
   var month = date.getMonth().toLocaleString("en-CA", {timeZone: "America/Toronto"});
 if (day ==  21 && month == 8) {
-    document.getElementById("H").innerHTML = "<h1>Happy birthday αqualine!</h1>"
+    H.innerHTML = "<h1>Happy birthday αqualine!</h1>"
     document.title = "Happy birthday!";
+    body.style.backgroundImage = "url('image2.png')";
   } else
   {
-    document.getElementById("H").innerHTML = "<h1>It's not αqualine's birthday</h1>"
+    H.innerHTML = "<h1>It's not αqualine's birthday</h1>"
     document.title = "αqualine's birthday website";
   }
